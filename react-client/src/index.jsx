b98e0b8a77bcb742240da6f0e1369fc04730a210
Sue Park <cherishgreentea@gmail.com>
2018-10-31 10:00:05 -0700

    Basic components for starting

---
+++ react-client/src/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+
+// Entry Point
+ReactDOM.render(<App />, document.getElementById('app'));
